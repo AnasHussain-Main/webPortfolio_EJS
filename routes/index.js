9e2469f94785d0813ec1bd919474920d25299d46
AnasHussain-Main <muhammad.hussain8@ontariotechu.net>
2024-11-02 19:31:13 -0400

    Added relevent webpage routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Home page route
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('home', { title: 'Home' });
+});
+
+// About Me page route
+router.get('/about', function(req, res, next) {
+    res.render('aboutMe', { title: 'About Me' });
+});
+
+// My Projects page route
+router.get('/projects', function(req, res, next) {
+    res.render('myProjects', { title: 'My Projects' });
+});
+
+// Contact Me page route
+router.get('/contact', function(req, res, next) {
+    res.render('contactMe', { title: 'Contact Me' });
 });
 
 module.exports = router;
