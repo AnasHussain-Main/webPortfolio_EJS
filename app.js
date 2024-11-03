c6f81731c69e9e2a4fdf7d59bdb54635d7a5c0a5
AnasHussain-Main <muhammad.hussain8@ontariotechu.net>
2024-11-03 01:28:10 -0400

    Added relevent layout files and packages

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts'); // Import express-ejs-layouts
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Set up express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layout');  // This sets 'layout.ejs' as the default layout for all views
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,3 +44,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
